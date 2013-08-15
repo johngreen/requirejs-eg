96ba8d0e5d26b1a55bd8f88737c84af7cf411db8
John Green <millenniumtoday@gmail.com>
2013-08-15 13:01:42 -0500

    added robots to require

--- js/useRobots.js
+++ js/useRobots.js
@@ -1,27 +1,3 @@
-require.config({
-    baseUrl: 'js',
-    paths: {
-        // the left side is the module ID,
-        // the right side is the path to
-        // the jQuery file, relative to baseUrl.
-        // Also, the path should NOT include
-        // the '.js' file extension. This example
-        // is using jQuery 1.9.0 located at
-        // js/lib/jquery-1.9.0.js, relative to
-        // the HTML page.
-        jquery: '//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery'
-    }
-});
-
-
-require(["jquery","bootMeUp","useRobots"], function($) {
-    //the jquery.alpha.js and jquery.beta.js plugins have been loaded.
-    $(function() {
-       
-    });
-});
-
-/*
 require(["jquery","person","android","doctor","robot","useRobots"], function($,person,android,doctor,robot) {
     //the jquery.alpha.js and jquery.beta.js plugins have been loaded.
     $(function() {
@@ -48,6 +24,3 @@ require(["jquery","person","android","doctor","robot","useRobots"], function($,p
        
     });
 });
-
-
-*/
